01561022a83a342afbd26e2ceb3e31b9e383886b
Martin Schibel <mshd94@gmail.com>
2020-05-10 12:34:16 +0200

    add icons #2

--- controllers/processNode.js
+++ controllers/processNode.js
@@ -192,9 +192,12 @@ function getPeopleData(claims, newClaims, treeType) {
     }
     var socialMedia = {
         'P6634' : ['linkedin','https://www.linkedin.com/in/$1/'],
-        'P2003' : ['instagram',' https://www.instagram.com/$1/'],
-        'P2002' : ['twitter','  https://twitter.com/$1'],
-        'P2013' : ['facebook',' https://www.facebook.com/$1/'],
+        'P2003' : ['instagram','https://www.instagram.com/$1/'],
+        'P2002' : ['twitter','https://twitter.com/$1'],
+        'P2013' : ['facebook','https://www.facebook.com/$1/'],
+        'P2949' : ['wikitree','https://www.wikitree.com/wiki/$1'],
+        'P2600' : ['geni',' https://www.geni.com/profile/index/$1'],
+        'P7085' : ['tiktok', ' https://www.tiktok.com/@$1'],
         // 'P345' : ['imdb',' https://www.imdb.com/name/$1/']
 
     };
