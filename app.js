28ccacdf86e1b826df1d2e2ca931b2fdaa431075
mshd <mshd94@gmail.com>
2020-05-13 20:41:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
